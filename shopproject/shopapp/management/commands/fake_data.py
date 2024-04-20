c11ec545d8d55b99dec7012265f03f58414fc06b
aleksey.krasnov <krasnov.80@mail.ru>
2024-04-20 10:40:10 +0300

    Add client

---
+++ shopproject/shopapp/management/commands/fake_data.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from shopapp.models import Client
+from random import randint
+
+
+class Command(BaseCommand):
+    help = 'Create client'
+
+    def handle(self, *args, **options):
+        num = randint(1, 1000)
+        phone = randint(1,10000000000)
+        new_client = Client(name_client=f'Alex{num}',email=f'Alex{num}@m.ru',
+                            number_phone=f'{phone}',address=f'City-{num}',
+                            registration_date_client='2024-04-20')
+        new_client.save()
+
+
+
